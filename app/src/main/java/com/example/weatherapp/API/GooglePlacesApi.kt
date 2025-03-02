d549a6297bcefdededb3604e85aedea445efd998
Ricu Alexandru Razvan <146858237+HolyCrusader2004@users.noreply.github.com>
2025-03-02 13:38:15 +0200

    Update GooglePlacesApi.kt

--- app/src/main/java/com/example/weatherapp/API/GooglePlacesApi.kt
+++ app/src/main/java/com/example/weatherapp/API/GooglePlacesApi.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Query
 interface GooglePlacesApi {
     @GET("maps/api/place/autocomplete/json")
     suspend fun getPredictions(
-        @Query("key") key: String ="AIzaSyC25QoPoIDkg5iXTnVzv6HYXypbEBjewWA",
+        @Query("key") key: String ="YOUR_API_KEY",
         @Query("types") types: String = "address",
         @Query("input") input: String
     ): GooglePredictionResponse
